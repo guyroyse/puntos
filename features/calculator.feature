5564f41bac7fb67104d40a22e71af1da9c22d4b8
Guy Royse <guy@guyroyse.com>
2010-12-05 17:47:01 -0500

    Added a simple webpage and server that Celerity can hit

--- features/calculator.feature
+++ features/calculator.feature
@@ -13,5 +13,5 @@ Feature: Points Calculator
     And my food has 5 grams of protein
     And my food has 5 grams of carbs
     And my food has 5 grams of fat
-    And my food has 30 grams of fiber
+    And my food has 40 grams of fiber
     Then my food should have a points value of 0
--- features/pages/calculator.rb
+++ features/pages/calculator.rb
@@ -4,8 +4,9 @@ class CalculatorPage
     @browser = browser
   end
   def visit
+    @browser.goto 'http://localhost:4567/calculator.html'
   end
   def points
-    (16 * @protein.to_f + 19 * @carbs.to_f + 45 * @fat.to_f - 14 * @fiber.to_f) / 175 
+    (16 * @protein.to_f + 19 * @carbs.to_f + 45 * @fat.to_f - 14 * @fiber.to_f) / 175
   end
 end
